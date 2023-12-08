0a705618b0ae5745a7c175299e576121e967ab7e
Dirk Debrunner <dirk.debrunner@liip.ch>
2023-12-08 15:52:13 +0100

    Working js.

--- js/commerce_google_tag_manager.js
+++ js/commerce_google_tag_manager.js
@@ -2,32 +2,38 @@
  * @file
  * Defines Javascript behaviors for the commerce_google_tag_manager module.
  */
-
 (function (Drupal, drupalSettings) {
-
-    Drupal.behaviors.datalayer_push = {
-      attach: function (context, settings) {
-        if (!drupalSettings) {
-          return;
-        }
-
-        var cgtmSettings = drupalSettings.commerceGoogleTagManager || {};
-        var url = cgtmSettings.eventsUrl;
-        var dataLayerVariable = cgtmSettings.dataLayerVariable;
-
-        if (!dataLayerVariable || !window.hasOwnProperty(dataLayerVariable)) {
-          return;
-        }
-
-        var dataLayer = window[dataLayerVariable];
-
-        fetch(url).then((data) => {
+  Drupal.behaviors.datalayer_push = {
+    attach: function (context, settings) {
+      if (!drupalSettings) {
+        return;
+      }
+      var cgtmSettings = drupalSettings.commerceGoogleTagManager || {};
+      var url = cgtmSettings.eventsUrl;
+      var dataLayerVariable = cgtmSettings.dataLayerVariable;
+      if (!dataLayerVariable || !window.hasOwnProperty(dataLayerVariable)) {
+        return;
+      }
+      var dataLayer = window[dataLayerVariable];
+      fetch(url)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Failed to fetch data");
+          }
+        })
+        .then((data) => {
           if (data && data.length) {
-            data.forEach(function(eventData) {
+            console.log(dataLayer);
+            data.forEach(function (eventData) {
               dataLayer.push(eventData);
             });
           }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
         });
-      }
-    };
+    },
+  };
 })(Drupal, drupalSettings);
